ae0f5b3ea4bab67b9fb675f3ad10dec5de98f3fb
frannieziesemer <52860762+frannieziesemer@users.noreply.github.com>
2020-02-18 16:39:49 +0100

    Fix timestamp

--- src/Weather.js
+++ src/Weather.js
@@ -12,18 +12,18 @@ export default function Weather(props) {
     event.preventDefault();
     search();
   }
+
+  function updatedCity(event) {
+    setCity(event.target.value);
+  }
+
   function search() {
     let apiKey = "498800558a7c5bd6e9bc3ec1ccb9adfe";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
     axios.get(url).then(searchWeather);
   }
 
-  function updatedCity(event) {
-    setCity(event.target.value);
-  }
-
   function searchWeather(response) {
-    setLoaded(true);
     setWeather({
       description: response.data.weather[0].main,
       timestamp: new Date(response.data.dt * 1000),
@@ -31,8 +31,11 @@ export default function Weather(props) {
       maximumTemp: Math.round(response.data.main.temp_max),
       minimumTemp: Math.round(response.data.main.temp_min),
       wind: Math.round(response.data.wind.speed),
-      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      //icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      city: response.data.name
     });
+    setLoaded(true);
+
     console.log(response.data);
   }
 
